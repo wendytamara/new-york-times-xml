a8624d883bcb31d41b4f5ad128516461ea5f3d4d
wendytamara <anacarlavegam@gmail.com>
2018-08-06 18:51:59 -0500

    corrigiendo error de servidor

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ form.addEventListener('submit', function(e) {
 
 function getNews() {
   const articleRequest = new XMLHttpRequest();
-  articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=974c1989f2064ce996936f08935aa84b`);
+  articleRequest.open('GET', `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=974c1989f2064ce996936f08935aa84b`);
   articleRequest.onload = addNews;
   articleRequest.onerror = handleError;
   articleRequest.send();
